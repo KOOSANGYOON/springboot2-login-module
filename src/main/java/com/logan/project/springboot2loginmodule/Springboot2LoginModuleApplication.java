d3b9163969bd4d87382b17b37ebb8c5fe713baa8
logan <logan.koo@kakaoix.com>
2020-07-18 12:41:08 +0900

    Initial commit

---
+++ src/main/java/com/logan/project/springboot2loginmodule/Springboot2LoginModuleApplication.java
@@ -0,0 +1,13 @@
+package com.logan.project.springboot2loginmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2LoginModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2LoginModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logan/project/springboot2loginmodule/Springboot2LoginModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logan.project.springboot2loginmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2LoginModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
